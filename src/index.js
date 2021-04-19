d99e911e5c6a21b7694bad34385fc8003da48154
Melissa Lawrence <mlawrence537@gmail.com>
2021-04-19 16:37:36 -0400

    worked on display hike

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const hikeNamesDiv = document.querySelector('div#hike-names')
+const displayDiv = document.querySelector('div#display-div')
 
 function renderOneName(hikeObj) {
     const nameSpan = document.createElement('span')
+    nameSpan.classList.add('hike-name-span')
+    nameSpan.dataset.id = hikeObj.id
 
     nameSpan.textContent = hikeObj.name 
 
@@ -13,5 +16,36 @@ function renderAllNames() {
         .then(response => response.json())
         .then(hikesArr => hikesArr.forEach(renderOneName))
 }
+
+hikeNamesDiv.addEventListener('click', event => {
+    let currentId = event.target.dataset.id
+    if (event.target.className === 'hike-name-span') {
+        getHikeInfo(currentId)
+    }
+})
+
+function getHikeInfo(id) {
+    fetch(`http://localhost:3000/hikes/${id}`)
+        .then(response => response.json())
+        .then(objInfo => displayHike(objInfo))
+}
+
+function displayHike(hikeObj) {
+    const displayName = document.querySelector('h2.display-name')
+    const displayImg = document.querySelector('img.display-img')
+    const displayLocation = document.querySelector('h4.display-location')
+    const displayDifficulty = document.querySelector('p.display-difficulty')
+    const displayDistance = document.querySelector('p.display-distance')
+    const displayRating = document.querySelector('img.display-rating')
+
+    displayName.textContent = hikeObj.name
+    displayImg.src = hikeObj.image
+    displayImg.alt = hikeObj.name 
+    displayLocation.textContent = hikeObj.location 
+    displayDifficulty.textContent = `Difficulty: ${hikeObj.difficulty}`
+    displayDistance.textContent = `Distance: ${hikeObj.distance} miles`
+    
+}
+
 
 renderAllNames()
